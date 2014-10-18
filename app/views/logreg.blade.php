4ee4eeea0f6edc2f73ffd8626e9a9267a582924c
Yinbo Liu (Tom) <tombo@sina.com>
2014-10-18 21:29:09 +0800

    logreg

---
+++ app/views/logreg.blade.php
@@ -0,0 +1,83 @@
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-1 column"></div>
+		<div class="col-md-5 column">
+			<img alt="140x140" src="/i/qvod.jpg" />
+		</div>
+		<div class="col-md-5 column">
+			<div class="tabbable" id="mytab">
+				<ul class="nav nav-tabs">
+					<li class="active">
+						<a href="#login" data-toggle="tab">登录</a>
+					</li>
+					<li>
+						<a href="#reg" data-toggle="tab">注册</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="login">
+						<br/>
+						<br/>
+						<br/>
+						<div class="row clearfix">
+							<div class="col-md-4 column text-right">
+								<label>用 户 名</label>
+								<br/>
+								<br/>
+								<label>密&nbsp&nbsp&nbsp&nbsp码</label>
+							</div>
+							<div class="col-md-2 column"></div>
+							<div class="col-md-4 column text-left">
+								<input type="text" name="lname" id="lname" />
+								<br/>
+								<br/>
+								<input type="password" name="lpass" id="lpass" />
+							</div>
+							<div class="col-md-2 column"></div>
+						</div>
+						<br/>
+						<div class="text-center"><button type="button" class="btn btn-primary">登录</button></div>
+						<br/>
+					</div>
+					<div class="tab-pane" id="reg">
+						<br/>
+						<br/>
+						<br/>
+						<div class="row clearfix">
+							<div class="col-md-4 column text-right">
+								<label>用 户 名</label>
+								<br/>
+								<br/>
+								<label>密&nbsp&nbsp&nbsp&nbsp码</label>
+								<br/>
+								<br/>
+								<label>确认密码</label>
+								<br/>
+								<br/>
+								<label>邮&nbsp&nbsp&nbsp&nbsp箱</label>
+							</div>
+							<div class="col-md-2 column"></div>
+							<div class="col-md-4 column text-left">
+								<input type="text" name="rname" id="rname" />
+								<br/>
+								<br/>
+								<input type="password" name="rpass" id="rpass" />
+								<br/>
+								<br/>
+								<input type="password" name="rpass2" id="rpass2" />
+								<br/>
+								<br/>
+								<input type="text" name="email" id="email" />
+							</div>
+							<div class="col-md-4 column"></div>
+						</div>
+						<br/>
+						<div class="text-center"><button type="button" class="btn btn-danger">注册</button></div>
+						<br/>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-1 column"></div>
+	</div>
+</div>
