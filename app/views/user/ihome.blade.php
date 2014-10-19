848ba0ad1895cac91d2f2aa0bc3531a8b6629e07
zhouys92 <zhouys_92@126.com>
2014-10-19 19:02:59 +0800

    个人主页
    主页的布局

---
+++ app/views/user/ihome.blade.php
@@ -0,0 +1,165 @@
+<div class="container">
+	<div class="row clearfix">		
+	<!--VideoList--!>
+		<div class ="col-md-8">
+			<!--已上传Title--!>
+			<div class="row clearfix">
+				<div class="col-md-12">
+					<div align="left">已发布视频</div>
+				</div>
+			</div>
+			<!--已上传Image--!>
+			<div class="row clearfix">
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>				
+					<div align="center">播放量：14万</div>	
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">	
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div> 
+			</div>
+			<div class="row clearfix">
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>				
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">	
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div> 
+			</div>
+			<div class="col-xs-12 divider img-rounded"></div>
+			<!--已分享Title--!>
+			<div class="row clearfix">
+				<div class="col-md-12">
+					<div align="left">已分享视频</div>
+				</div>
+			</div>
+			<!--已分享Image--!>
+			<div class="row clearfix">
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>				
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">	
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div> 
+			</div>
+			<div class="row clearfix">
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">	
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div> 
+			</div>
+			<div class="col-xs-12 divider img-rounded"></div>	
+			<!--系统推荐Title--!>
+			<div class="row clearfix">
+				<div class="col-md-12">
+					<div align="left">推荐视频</div>
+				</div>
+			</div>
+			<!--系统推荐Image--!>
+			<div class="row clearfix">
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">						  
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					<div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div>
+				<div class="col-md-3">	
+					<img src="*.jpg" style="width:100%;height:auto;"/>
+					 <div align="center">Wild China</div>
+					<div align="center">播放量：14万</div>
+				</div> 
+			</div>	
+		</div>
+		<!--Info--!>
+		<div class="col-md-4">
+			<div class="row clearfix">
+				<div class="col-md-12">
+					<div align="left">个人信息</div>
+				</div>			
+			</div>		
+			<div class="row clearfix">
+				<div class="col-md-6">
+					<div align=left> 昵称：</div>
+				</div>
+				<div class="col-md-6">
+					<div align=left>头像：</div>
+					<div align=center>
+						<img src="*.jpg" style="width:80%;height:auto;"/>
+					</div>
+				</div>
+			</div>	
+			<div class="row clearfix">
+				<div class=col-md-12">
+					<div align="left">个人简介：</div>
+					<div align="left">复旦大学（英语：Fudan University，缩写：FDU），简称復旦，旧称復旦公學、國立復旦大學，创建于1905年，是中国人自主创办的第一所高等学校[2]。位于中国上海市，是被认为仅次于北京大学、清华大学的华东五所大学（即：华东五校）之一，中國著名大學，名列首批211工程、首批985工程名单。现已发展成一所国际著名的综合性、研究型高校，目标是建设世界一流大学[3]。“复旦”的字面意思是“旦复旦兮（日复一日）”，表示取之不尽，用之不竭的自力更生和勤奋[4]。				</div>
+			</div>
+			<div align="left">已发布视频数：20个</div>
+		</div>
+	</div>		
+</div>	
