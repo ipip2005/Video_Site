c3ff076c9974e8f5bce642aab65e28781492023a
Yinbo Liu (Tom) <tombo@sina.com>
2014-10-19 18:32:16 +0800

    flowplayer

---
+++ app/views/video.blade.php
@@ -0,0 +1,66 @@
+{{ HTML::style('css/minimalist.css') }}
+{{ HTML::script('js/flowplayer.min.js') }}
+<div>TITLE</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-8">
+			<div class="flowplayer" style="background-color:black;width:100%;height:450px;">
+				<video src="/i/movie.ogg">
+					您的浏览器不支持播放插件
+				</video>
+			</div>	
+		</div>
+		<div class="col-md-4">
+			<div style="height:150px">视频介绍：一只小熊正在过河。好可爱啊好可爱！！</div>
+			<br/>
+			<br/>
+			<div>上传者：ipip</div>
+			<br/>
+			<br/>
+			<div>点击量：200</div>
+			<br/>
+			<br/>
+			<div>☆☆☆☆☆</div>
+			<br/>
+			<br/>
+			<div><button class="btn btn-primary">分享</button></div>
+		</div>
+	</div>
+	<div style="height:25px"></div>
+	<div class="row">
+		<div class="col-md-8">
+			<div class="input-group">
+				<input type="text"	class="form-control search-content" placeholder="有什么想说的吗？快来评论吧！">
+				<div class="input-group-btn">
+				    <button class="btn btn-primary ">评论</button>
+				</div>
+			</div>
+			<div style="height:50px"></div>
+			<div>
+				<div class="row">
+					<div class="col-md-3 text-center"><img style="width:80%" alt="140x140" src="/i/qvod.jpg" class="img-rounded"/></div>
+					<div class="col-md-9"><div><a href="baidu.com">ipip</a></div>好好看啊好好看！</div><br/><div>30分钟前</div>
+				</div>
+				————————————————————————————————————————————————————
+				<div class="row">
+					<div class="col-md-3 text-center"><img style="width:80%" alt="140x140" src="/i/qvod.jpg" class="img-rounded"/></div>
+					<div class="col-md-9"><div><a href="baidu.com">ipip</a></div>我来看看</div><br/><div>50分钟前</div>
+				</div>
+				————————————————————————————————————————————————————
+			</div>
+		</div>
+		<div class="col-md-4">
+			为您推荐：
+			<div class="row">
+				<div class="col-md-4 text-center"><img style="width:80%" alt="140x140" src="/i/qvod.jpg" class="img-rounded"/></div>
+				<div class="col-md-8"><div><a href="baidu.com">快播1</a></div></div>
+			</div>
+			————————————————————————————————————————————————————
+			<div class="row">
+				<div class="col-md-4 text-center"><img style="width:80%" alt="140x140" src="/i/qvod.jpg" class="img-rounded"/></div>
+				<div class="col-md-8"><div><a href="baidu.com">快播2</a></div></div><br/>
+			</div>
+			————————————————————————————————————————————————————
+		</div>
+	</div>
+	</div>
