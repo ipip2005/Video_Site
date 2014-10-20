54acf06969ce5995e7ae78c6fdfd3e382f689afd
ipip2005 <supersingerman@126.com>
2014-10-20 20:52:15 +0800

    group table加 user_id

--- app/views/user/upload.blade.php
+++ app/views/user/upload.blade.php
@@ -1,57 +1,31 @@
-<div class = "container">
-	<div class ="row clearfix">
-		<div class="col-md-12">
-			<ul class="nav nav-tabs">
-                <li class="active">
-					<a href="#Podcast">已发布视频</a>
-                </li>
-                <li>
-                    <a href="#Upload">上传视频</a>
-                </li>
-            </ul>
-            <div id="myTabContent" class="tab-content">
-                <div class="tab-pane fade in active" id ="Podcast">
-                    <div class="container">
-						<div class="row clearfix">
-                            <div class="col-md-3">
-                                <img src="*.jpg" style="width:100%;height:auto;">
-                            </div>
-                            <div class="col-md-6">
-                                <div class="row clearfix">
-                                    <div class="col-md-3">
-                                          视频名称：
-                                    </div>
-                                    <div class="col-md-6">
-										Wild China
-                                    </div>
-									<div class="col-md-3">
-                                        修改
-                                    </div>
-								</div>
-								<div class="row clearfix">
-                                    <div class="col-md-3">
-                                          视频简介：
-                                    </div>
-                                    <div class="col-md-6">
-                                          《美丽中国》是一部有关中国自然主题的6集自然纪录
-    片，由英国广播公司自然历史部、中视传媒与Travel Channel合作拍摄完成，整部影片采用高清格>    式制作。2008年5月11日在英国广播公司第二台首播。英国广播公司持有本片在英国的著作权，其他
-    地区著作权由中视传媒持有。
-									</div>
-									<div class="col-md-3">
-                                        修改
-                                    </div>
-								</div>  
-							</div>
-							<div class="col-md-3">
-								删除
-							</div>
-						</div>
+<ul class="nav nav-tabs">
+	<li class="active"><a href="#Podcast">已发布视频</a></li>
+	<li ><a href="#Upload">上传视频</a></li>
+</ul>
+<div id="myTabContent" class="tab-content">
+	<div class="tab-pane fade in active" id="Podcast">
+		<div class="container">
+			<div class="row clearfix">
+				<div class="col-md-3">
+					<img src="*.jpg" style="width: 100%; height: auto;">
+				</div>
+				<div class="col-md-6">
+					<div class="row clearfix">
+						<div class="col-md-3">视频名称：</div>
+						<div class="col-md-6">Wild China</div>
+						<div class="col-md-3">修改</div>
+					</div>
+					<div class="row clearfix">
+						<div class="col-md-3">视频简介：</div>
+						<div class="col-md-6">《美丽中国》是一部有关中国自然主题的6集自然纪录
+							片，由英国广播公司自然历史部、中视传媒与Travel Channel合作拍摄完成，整部影片采用高清格>
+							式制作。2008年5月11日在英国广播公司第二台首播。英国广播公司持有本片在英国的著作权，其他 地区著作权由中视传媒持有。</div>
+						<div class="col-md-3">修改</div>
 					</div>
 				</div>
-				<div class="tab-pane fade in active" id ="Upload">
- 
-                </div>
+				<div class="col-md-3">删除</div>
 			</div>
-        </div>
-    </div> 
- </div>
+		</div>
+	</div>
+	<div class="tab-pane fade in active" id="Upload"></div>
+</div>
